ee9ccd933176bd9b34ba69c9dac47e9268856eae
andrew_guk <gukandrey91@gmail.com>
2026-06-27 15:01:45 +0300

    add routes for MATCH_WEEKS

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -20,9 +20,17 @@ export const TEAMS_ROUTES = {
     ARSENAL: 'arsenal',
 } as const
 
-export const GW_ROUTES = {
-    GW1: 'gameWeek1',
-    GW2: 'gameWeek2',
+export type MatchWeekId = `mw${number}`;
 
-}
 
+export const WEEKS_COUNT = 14;
+export const MATCH_WEEKS_IDS = Array.from(
+    { length: WEEKS_COUNT },
+    (_, i) => `mw${i + 1}` as MatchWeekId
+);
+
+// Результат: { mw1: 'match-week-1', mw2: 'match-week-2', ... }
+export const MW_ROUTES = MATCH_WEEKS_IDS.reduce((acc, id, index) => {
+    acc[id] = `match-week-${index + 1}`;
+    return acc;
+}, {} as Record<MatchWeekId, string>);
