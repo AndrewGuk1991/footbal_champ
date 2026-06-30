455e52cd147e84e07fc0a906b749bdb00aac4dd2
andrew_guk <gukandrey91@gmail.com>
2026-06-30 19:06:09 +0300

    delete SideBar and refactoring Header

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,20 +1,25 @@
-import {Box} from '@mui/material';
-import {SideBar} from "./components/SideBar.tsx";
-import {Footer} from "./components/Footer.tsx";
-import {Main} from "./components/Main.tsx";
-import {Header} from "./components/Header.tsx";
-
+import { Box } from '@mui/material';
+import { Footer } from "./components/Footer.tsx";
+import { Main } from "./components/Main.tsx";
+import { Header } from "./components/Header.tsx";
 
 export const Layout = () => {
-
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
-            <Header/>
-            <Box sx={{display: 'flex', flexGrow: 1, pt: ['56px', '64px']}}>
-                <SideBar/>
-                <Main/>
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Header />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexGrow: 1,
+                    pt: ['56px', '64px'], // Компенсация высоты фиксированного Header
+                    justifyContent: 'center', // Центрирует содержимое по горизонтали
+                    alignItems: 'flex-start'  // Прижимает контент к верху для правильного скролла
+                }}
+            >
+                <Main />
             </Box>
-            <Footer/>
+
+            <Footer />
         </Box>
     );
 };
