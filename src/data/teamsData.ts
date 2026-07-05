6078a09df0664823299d0b4534695bd59272eed2
andrew_guk <gukandrey91@gmail.com>
2026-07-05 22:01:42 +0300

    refactoring teamsData

--- src/data/teamsData.ts
+++ src/data/teamsData.ts
@@ -1,3 +1,4 @@
+import player_8 from './../assets/photoPlayers/photo_8.jpg'
 
 export type Player = {
     name: string;
@@ -53,8 +54,8 @@ export const TEAMS_DATA: Record<string, TeamData> = {
         midfielders: [
             { name: 'Марк', surname: 'Кот', number: 6, games: 14 },
             { name: 'Владимир', surname: 'Сидоревич', number: 7, goals: 4, games: 14 },
-            { name: 'Андрей', surname: 'Гук', number: 8, games: 14, yellowCards: 1 },
-            { name: 'Евгенний', surname: 'Радионов', number: 11, games: 14 },
+            { name: 'Андрей', surname: 'Гук', number: 8, games: 14, yellowCards: 1, photo: player_8, goals: 2 },
+            { name: 'Евгенний', surname: 'Радионов', number: 11, games: 14, goals: 2 },
         ],
         forwards: [
             { name: 'Вадим', surname: 'Шикула', number: 12, games: 14 },
